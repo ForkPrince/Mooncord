379cc0056aa901256c0fdb6c2697717e8ff3d7d2
Prince527 <Prince527gaming@gmail.com>
2024-07-16 11:58:03 -0300

    Update settings.tsx

--- src/plugins/_core/settings.tsx
+++ src/plugins/_core/settings.tsx
@@ -105,7 +105,7 @@ export default definePlugin({
             },
             {
                 section: "SuncordSettings",
-                label: "Suncord",
+                label: "Settings",
                 element: VencordTab,
                 className: "vc-settings"
             },
