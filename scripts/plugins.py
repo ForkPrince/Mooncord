63b3d7ef1417843b93efdc6383edbcc0bb6e3b02
Prince527 <Prince527gaming@gmail.com>
2024-07-18 04:00:10 -0300

    Fixed my python skills

--- scripts/plugins.py
+++ scripts/plugins.py
@@ -8,15 +8,13 @@ directory = os.path.dirname(__file__)
 def getPlugins(source, output = "custom", plugins = None, folder = "plugins"):
     temp = "./temp"
 
-    folder = os.path.join(directory, "../src/", output)
-
-    os.makedirs(folder, exist_ok=True)
+    os.makedirs(output, exist_ok=True)
 
     try:
         subprocess.run(["git", "clone", source, temp], check=True)
         for plugin in plugins:
             src_path = os.path.join(temp, "src", folder, plugin)
-            dest_path = os.path.join(folder, plugin)
+            dest_path = os.path.join(directory, "../src/", output, plugin)
 
             if os.path.exists(src_path):
                 shutil.move(src_path, dest_path)
